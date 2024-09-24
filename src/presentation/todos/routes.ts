7c1f2624996e4e74a5a13bea8d7db365b368cf82
DeveTe5t <devcurses@gmail.com>
2024-09-24 10:20:42 -0600

    rest server and crud

---
+++ src/presentation/todos/routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { TodosController } from "./controller";
+
+
+export class TodoRoutes {
+
+    static get routes(): Router {
+        const router = Router();
+        const todoController = new TodosController();
+
+        // router.get('/api/todos', (req, res) => todoController.getTodos(req, res));
+        // similar but not necesary because is parametes in the same order
+        router.get('/', todoController.getTodos);
+        router.get('/:id', todoController.getTodoById);
+        router.post('/', todoController.createTodo);
+        router.put('/:id', todoController.updateTodo);
+        router.delete('/:id', todoController.deteTodo);
+
+        return router;
+    }
+}
