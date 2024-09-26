8c821c4b19fe49736f0f866e20358dc00dbbc64d
DeveTe5t <devcurses@gmail.com>
2024-09-26 14:54:15 -0600

    Add variable to envs in config folder

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,4 +7,5 @@ import { get } from 'env-var';
 export const envs = {
     PORT: get('PORT').required().asPortNumber(),
     PUBLIC_PATH: get('PUBLIC_PATH').required().default('public').asString(),
+    POSTGRES_URL: get('POSTGRES_URL').default('public').asString(),
 }
